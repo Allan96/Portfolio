d07289699fdf7dc25a90e74f93a51243591e8299
Allan Souza <allan.souza.silva@usp.br>
2019-05-30 00:15:47 -0300

    Modificações

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,7 +8,7 @@ $.ajax({
         while (i < data.length){
             $('.repo-add').append(
                 '<a href="' + data[i]['html_url'] + 
-                '" target="_blank" class="col-12 col-sm-3"><div class="repo-box"><p class="bold upper mb-0 repo-title c-white">' + 
+                '" target="_blank" class="col-12 col-sm-6"><div class="repo-box"><p class="bold upper mb-0 repo-title c-white">' + 
                 data[i]['name'] + '</p><p class="c-white repo-desc">' + 
                 data[i]['description'] + '</p></div></a>'
             );
@@ -21,14 +21,14 @@ $.ajax({
 
 $('.second').click(function (e) { 
     e.preventDefault();
-    $('#first').fadeOut('fast');
-    $('#second').fadeIn('1000');
+    $('#first').hide();
+    $('#second').fadeIn(1000);
     
 });
 
 $('.first').click(function (e) { 
     e.preventDefault();
-    $('#second').fadeOut();
-    $('#first').fadeIn('1000');
+    $('#second').hide();
+    $('#first').fadeIn(1000);
     
 });
